88db0dd49575162ac7ba20f7accdb631cb2649db
Nathan Lee <leen7@spu.edu>
2020-02-24 15:15:57 -0800

    added clock

--- MultiFunctionalDashboard/mainwindow.cpp
+++ MultiFunctionalDashboard/mainwindow.cpp
@@ -3,13 +3,31 @@
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    , ui(new Ui::MainWindow),
+      timer(new QTimer)
 {
     ui->setupUi(this);
+
+    connect(timer, SIGNAL(timeout()),
+            this, SLOT(setCurrentTime()));
+    setCurrentTime();
+    timer->start(1000);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::setCurrentTime()
+{
+    QTime time = QTime::currentTime();
+    QString hour = time.toString("hh");
+    QString minute = time.toString("mm");
+    QString second = time.toString("ss");
+
+    ui->hourValue->display(hour);
+    ui->minuteValue->display(minute);
+    ui->secondValue->display(second);
 }
 
